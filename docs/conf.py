640af3566f3bca21890710cf90b2afab5c1229c2
Eric Lo <lxneng@gmail.com>
2011-11-29 14:25:21 +0800

    update

--- docs/conf.py
+++ docs/conf.py
@@ -1,31 +1,9 @@
-# -*- coding: utf-8 -*-
-import sys, os
-
-sys.path.append(os.path.abspath('_themes'))
-
-templates_path = ['_templates']
-
 source_suffix = '.rst'
-
 master_doc = 'index'
-
 project = u'Style-Guidelines'
 copyright = u'2011, lxneng.com'
-
 version = '0.1'
 release = '0.1'
-
-exclude_patterns = ['_build']
-html_theme_path = ['_themes']
-html_static_path = ['_static']
-html_title = None
-html_theme = 'flask'
-html_theme_options = {
-            'index_logo': 'logo.png',
-            }
-html_sidebars = {
-    'index': ['fork-me-on-github.html', 'localtoc.html', 'searchbox.html'],
-    '**': ['localtoc.html', 'relations.html', 'searchbox.html'],
-}
-
-htmlhelp_basename = 'Style-Guidelines'
+html_theme = 'nature'
+html_title = 'A collection of coding style guidelines' 
+htmlhelp_basename = 'lxneng.com'
